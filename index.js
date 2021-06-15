233dc389498ae540d7acb01c176531dad6412348
Daban Sadiq <dabansadiq@icloud.com>
2021-06-15 12:06:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ function findMatching (list, name) {
     return driverName.toLowerCase() === name.toLowerCase();
   });
 }
-//
-// function fuzzyMatch (list, partialName) {
-//   let lengthOfName = partialName.length;
-//   return list.filter(function (driverName) {
-//     return driverName.slice(0, lengthOfName) === partialName;
-//   });
+
+function fuzzyMatch (list, partialName) {
+  let lengthOfName = partialName.length;
+  return list.filter(function (driverName) {
+    return driverName.slice(0, lengthOfName) === partialName;
+  });
 }
 
 function matchName (list, name) {
